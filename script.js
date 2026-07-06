a035c51eaaa687267a03b585c83a374fa9ab9598
BhavikGaur22 <102758356+BhavikGaur22@users.noreply.github.com>
2026-07-06 09:56:49 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 
-var typed=new Typed("#text", { 
-strings: ["Data Engineer","Data Scientist","AI Engineer"],
-typeSpeed:50,
-backSpeed:50,
-backDelay: 2000,
-loop:true
+var typed = new Typed("#text", {
+    strings: ["BHAVIK TEST"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 2000,
+    loop: true
 });
 
-// Select the About link in the navigation
 const aboutLink = document.querySelector('a[href="#about"]');
-
-// Select the About section
 const aboutSection = document.getElementById('about');
 
-// Smooth scroll to the About section when the link is clicked
 aboutLink.addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent default link behavior
+    event.preventDefault();
     aboutSection.scrollIntoView({ behavior: 'smooth' });
 });
