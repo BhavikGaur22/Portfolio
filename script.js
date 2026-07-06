cbc3a697db2291b146b89eb886f9db3c75d10a7e
BhavikGaur22 <102758356+BhavikGaur22@users.noreply.github.com>
2026-07-06 10:21:10 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-console.log("SCRIPT LOADED");
-var typed = new Typed("#text", {
-    strings: ["BHAVIK TEST"],
-    typeSpeed: 50,
+
+consol**log("SCRIPT LOADED");
+
+var typed **new Typed("#text", {
+    strings**["BHAVIK TEST"],
+    typeSpeed: 5**
     backSpeed: 50,
-    backDelay: 2000,
+    backDelay**2000,
     loop: true
-});
-
-const aboutLink = document.querySelector('a[href="#about"]');
-const aboutSection = document.getElementById('about');
-
-aboutLink.addEventListener('click', function(event) {
-    event.preventDefault();
-    aboutSection.scrollIntoView({ behavior: 'smooth' });
 });
